d6e3cf061de9d9c308c06b48d6e5f2c9f6873079
anluu <conganwoz@gmail.com>
2022-07-31 12:11:18 +0700

    setup postgres database

--- jinote_api/settings.py
+++ jinote_api/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'jinote_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'jinote',
+        'USER': 'anluu',
+        'PASSWORD': 'anluu',
+        'HOST': 'localhost',
+        'PORT': '5433'
     }
 }
 
