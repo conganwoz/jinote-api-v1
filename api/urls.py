5e2adfc4713bb1896ed6e4d6ea6e0874761d7347
anluu <conganwoz@gmail.com>
2022-08-09 14:10:49 +0700

    sync

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('hello/', views.say_hello)
+    path('hello/', views.say_hello),
+    path('upload_note/', views.upload_note)
 ]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+import json
+
 def say_hello(reuqest):
     return HttpResponse('Hello World')
+
+def upload_note(request):
+    print('in_here_')
+    print(request)
+    body = request.body.decode('utf-8')
+    body = json.loads(body)
+    content = body['content']
+    print(content)
+    return HttpResponse("ok")
 
