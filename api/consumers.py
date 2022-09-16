ad63636db2e5f2579a8be98e645d5e18b91d18f6
anluu <conganwoz@gmail.com>
2022-09-16 16:37:58 +0700

    update consumer for sync tunnel event

--- api/consumers.py
+++ api/consumers.py
@@ -1,4 +1,5 @@
 import json
+from nis import match
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
 
@@ -24,22 +25,66 @@ class ChatConsumer(WebsocketConsumer):
 
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        message = text_data_json['message']
+        message_type = text_data_json['message_type']
+
+        if message_type == 'receiver_connected':
+          message = text_data_json['message']
+          async_to_sync(self.channel_layer.group_send)(
+              self.room_group_name,
+              {
+                  'type': 'chat_message',
+                  'message': message,
+                  'message_type': message_type
+              }
+          )
+          
+        elif message_type == 'sender_ice_candidate' or message_type == 'receiver_ice_candidate':
+          candidate = text_data_json['candidate']
+          async_to_sync(self.channel_layer.group_send)(
+              self.room_group_name,
+              {
+                  'type': 'chat_message',
+                  'candidate': candidate,
+                  'message_type': message_type,
+              }
+          )
+        elif message_type == 'sender_desc' or message_type == 'receiver_desc':
+          description = text_data_json['description']
+          async_to_sync(self.channel_layer.group_send)(
+              self.room_group_name,
+              {
+                  'type': 'chat_message',
+                  'description': description,
+                  'message_type': message_type,
+              }
+          )
+
+        else:
+          message = text_data_json['message']
+          async_to_sync(self.channel_layer.group_send)(
+              self.room_group_name,
+              {
+                  'type': 'chat_message',
+                  'message': message,
+                  'message_type': message_type,
+              }
+          )
 
         # Send message to room group
-        async_to_sync(self.channel_layer.group_send)(
-            self.room_group_name,
-            {
-                'type': 'chat_message',
-                'message': message
-            }
-        )
+        
 
     # Receive message from room group
     def chat_message(self, event):
-        message = event['message']
+        message = event.get('message')
+        message_type = event.get('message_type')
+        description = event.get('description')
+        candidate = event.get('candidate')
+
 
         # Send message to WebSocket
         self.send(text_data=json.dumps({
-            'message': message
+            'message': message,
+            'message_type': message_type,
+            'description': description,
+            'candidate': candidate
         }))
