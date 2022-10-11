9cc7389edd91bda248a74d0123dca95546c39596
Lưu Công An <42475170+conganwoz@users.noreply.github.com>
2022-10-11 11:31:39 +0700

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('hello/', views.say_hello),
     path('upload_note/', views.upload_note),
     path('download_notes/', views.download_notes),
     path('<str:room_name>/', views.room, name='room'),
